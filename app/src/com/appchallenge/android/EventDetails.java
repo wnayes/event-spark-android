4eb12d345744ed66bbbf6c517ddfffcd0611b26f
Will Nayes <wnayes@gmail.com>
2013-02-08 19:11:33 -0600

    Removed tab interface in EventDetails.java

--- app/src/com/appchallenge/android/EventDetails.java
+++ app/src/com/appchallenge/android/EventDetails.java
@@ -1,5 +1,7 @@
 package com.appchallenge.android;
 
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -11,56 +13,27 @@ import com.actionbarsherlock.view.MenuItem;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class EventDetails extends SherlockFragmentActivity implements EventDetailsInfoTab.InfoTabListener,
-                                                                      EventDetailsAttendanceTab.AttendanceTabListener,
-                                                                      EventDetailsLocationTab.LocationTabListener {
+public class EventDetails extends SherlockFragmentActivity {
+	// Private members containing the Event information.
 	private int id;
-	
-	// Methods implementing InfoTabListener
 	private String title;
-	public String getEventTitle() {
-		return this.title;
-	}
-	
 	private String description;
-	public String getEventDescription() {
-		return this.description;
-	}
-
 	private Date startDate;
-	public Date getEventStartDate() {
-		return this.startDate;
-	}
-
 	private Date endDate;
-	public Date getEventEndDate() {
-		return this.endDate;
-	}
-	
-	// Methods implementing AttendanceTabListener
 	private int attendance;
-	public int getEventAttendance() {
-		return this.attendance;
-	}
-	
-	// Methods implementing LocationTabListener
 	private LatLng location;
-	public LatLng getEventLocation() {
-		return this.location;
-	}
-
 	private LatLng userLocation;
-	public LatLng getUserLocation() {
-		return this.userLocation;
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,30 +56,46 @@ public class EventDetails extends SherlockFragmentActivity implements EventDetai
 		// The home button takes the user back to the map display.
 		ActionBar bar = getSupportActionBar();
 		bar.setDisplayHomeAsUpEnabled(true);
-
-		// Establish the tab navigation interface.
-		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		Tab attendTab = bar.newTab().setText("Attendance");
-		Tab infoTab = bar.newTab().setText("Info");
-		Tab locationTab = bar.newTab().setText("Location");
-
-		Fragment attendFragment = new EventDetailsAttendanceTab();
-		Fragment infoFragment = new EventDetailsInfoTab();
-		Fragment locationFragment = new EventDetailsLocationTab();
-
-		attendTab.setTabListener(new TabsListener(attendFragment));
-		infoTab.setTabListener(new TabsListener(infoFragment));
-		locationTab.setTabListener(new TabsListener(locationFragment));
-
-		bar.addTab(attendTab);
-		bar.addTab(infoTab);
-		bar.addTab(locationTab);
 		
-		if (savedInstanceState != null)
-			bar.selectTab(bar.getTabAt(savedInstanceState.getInt("currentTab", 1)));
-		else
-		    bar.selectTab(infoTab);
+		// Display the Event title and description.
+	    ((TextView)findViewById(R.id.event_details_title)).setText(this.title);
+	    TextView descBox = (TextView)findViewById(R.id.event_details_description);
+	    if (this.description.isEmpty()) {
+	    	descBox.setText(R.string.event_description_empty);
+	    	descBox.setTypeface(null, Typeface.ITALIC);
+	    }
+	    else {
+	    	descBox.setText(this.description);
+	    	descBox.setTypeface(null, Typeface.NORMAL);
+	    }
+	    
+	    // Display different date strings based on the time of the event.
+	    Calendar today = Calendar.getInstance();
+	    Calendar startCalendar = Calendar.getInstance();
+	    startCalendar.setTime(this.startDate);
+	    Calendar endCalendar = Calendar.getInstance();
+	    endCalendar.setTime(this.endDate);
+
+	    String dateString = "";
+	    if (endCalendar.before(today)) {
+	    	dateString += "This event has ended.";
+	    	((TextView)findViewById(R.id.event_details_date_description)).setText(dateString);
+	    }
+	    else {
+		    if (startCalendar.after(today)) {
+		    	if (startCalendar.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR))
+		    		dateString += "Begins today at " + DateFormat.getTimeInstance(DateFormat.SHORT).format(startCalendar.getTime()) + ". ";
+		    	else
+		    		dateString += "Begins tomorrow at " + DateFormat.getTimeInstance(DateFormat.SHORT).format(startCalendar.getTime()) + ". ";
+		    }
+	
+		    if (endCalendar.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR))
+	    		dateString += "Ends today at " + DateFormat.getTimeInstance(DateFormat.SHORT).format(endCalendar.getTime()) + ".";
+	    	else
+	    		dateString += "Ends tomorrow at " + DateFormat.getTimeInstance(DateFormat.SHORT).format(endCalendar.getTime()) + ".";
+	    }
+	    
+	    ((TextView)findViewById(R.id.event_details_date_description)).setText(dateString);
 	}
 
 	@Override
@@ -118,41 +107,27 @@ public class EventDetails extends SherlockFragmentActivity implements EventDetai
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	   switch (item.getItemId()) {
-	      case android.R.id.home:
-	    	 // Return to the EventViewer.
-	         finish();
-	         return true;
-	      default:
-	         return super.onOptionsItemSelected(item);
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	    	    // Return to the EventViewer.
+	            finish();
+	            return true;
+	        case R.id.menu_refresh_event:
+	        	
+	        	return true;
+	        case R.id.menu_get_directions:
+	        	// Prepare maps url query url parameters.
+            	String startCoords = ((Double)this.userLocation.latitude).toString() + "," + ((Double)this.userLocation.longitude).toString();
+            	String endCoords = ((Double)this.location.latitude).toString() + "," + ((Double)this.location.longitude).toString();
+            	String url = "http://maps.google.com/maps?saddr=" + startCoords + "&daddr=" + endCoords;
+            	Log.d("EventDetailsLocationTab", "Get directions, " + url);
+
+                // Pass an intent to an activity that can provide directions.
+            	Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
 	   }
-	}
-	
-	public void onSaveInstanceState(Bundle savedInstanceState) {
-		// Prevent the current tab from being lost on rotation
-		savedInstanceState.putInt("currentTab", getSupportActionBar().getSelectedTab().getPosition());
-		
-		super.onSaveInstanceState(savedInstanceState);
-	}
-
-	/**
-	 * A basic implementation of a TabListener which allows switching between tabs.
-	 */
-	class TabsListener implements ActionBar.TabListener {
-	    public Fragment fragment;
-
-	    public TabsListener(Fragment fragment) {
-	        this.fragment = fragment;
-	    }
-
-	    public void onTabReselected(Tab tab, FragmentTransaction ft) {}
-
-	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
-	        ft.replace(R.id.details_fragment_container, fragment);
-	    }
-
-	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-	        ft.remove(fragment);
-	    }
 	}
 }
