eedda21835ed566711451d7cc94e96651ef5a5bc
Will Nayes <wnayes@gmail.com>
2013-11-07 21:31:55 -0600

    Add new meta-data to manifest to fix runtime crash from Google Play Services. Unused import warning fix.

--- app/src/com/appchallenge/eventspark/EventDetails.java
+++ app/src/com/appchallenge/eventspark/EventDetails.java
@@ -44,7 +44,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.internal.view.menu.MenuBuilder;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
