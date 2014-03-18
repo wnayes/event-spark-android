f648d4ed0822c6dab83476afc3042bcbbf68a847
Will Nayes <wnayes@gmail.com>
2014-03-18 15:54:28 -0500

    Avoid posting entire token to LogCat.

--- app/src/com/appchallenge/eventspark/CreateEvent.java
+++ app/src/com/appchallenge/eventspark/CreateEvent.java
@@ -144,7 +144,7 @@ public class CreateEvent extends ActionBarActivity implements CreateEventInterfa
 
 	private String token;
 	public void setToken(String token) {
-		Log.d("CreateEvent.setToken", "Received token: " + token);
+		Log.d("CreateEvent.setToken", "Received token: " + token.substring(0, token.length() / 10) + "...");
 		this.token = token;
 	}
 
